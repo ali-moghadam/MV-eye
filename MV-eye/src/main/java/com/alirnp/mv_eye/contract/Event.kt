ded51ae2cc48a668299bf6ec173d9d0ba7560bb0
Ali Moghadam <ali.moghadam@snapp.cab>
2023-11-10 00:12:35 +0330

    Add library contracts

---
+++ MV-eye/src/main/java/com/alirnp/mv_eye/contract/Event.kt
@@ -0,0 +1,6 @@
+package com.alirnp.mv_eye.contract
+
+/**
+ * Defines the contract for event classes.
+ */
+interface Event
---
+++ MV-eye/src/main/java/com/alirnp/mv_eye/contract/UiState.kt
@@ -0,0 +1,7 @@
+package com.alirnp.mv_eye.contract
+
+/**
+ * Defines the contract for UI state classes.
+ * Data classes are the preferred way to define UI state classes.
+ */
+interface UiState
